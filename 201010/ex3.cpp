9f660ab956b2f58ad870ce3498d031b2ae17baa4
kwarkmc <kwarkmc@naver.com>
2020-10-10 11:26:24 +0000

    201010 20:26 상속이 필요한 이유

---
+++ 201010/ex3.cpp
@@ -0,0 +1,73 @@
+// 메시지
+// 텍스트
+// 이미지
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Image {
+public:
+	operator string() {
+		return "사진";
+	}
+};
+
+class TextMessage {
+public:
+	TextMessage(int sendTime, string sendName, string text) {
+		this->sendTime = sendTime;
+		this->sendName = sendName;
+		this->text = text;
+	} 
+	
+	int GetSendTime() const {return sendTime;}
+	string GetSendName() const {return sendName;}
+	string GetText() const {return text;}
+	
+private:
+	int sendTime;
+	string sendName;
+	string text;
+};
+
+class ImageMessage {
+public:
+	ImageMessage(int sendTime, string sendName, Image *image) {
+		this->sendTime = sendTime;
+		this->sendName = sendName;
+		this->image = image; //포인터 자체를 얕은 복사하는 것은 문제가 되지 않는다.
+	} 
+	int GetSendTime() const {return sendTime;}
+	string GetSendName() const {return sendName;}
+	Image *GetImage() const {return image;}
+private:
+	int sendTime;
+	string sendName;
+	Image *image; //Image라는 클래스 자체가 ImageMessage 클래스 내로 들어가게 되면, ImageMessage 클래스가 아주 무거워지기 때문에, 포인터로 조작한다.
+	
+};
+
+int main() {
+	// 두들 : "안녕"
+	// 두들 : "강아지 사진"
+	
+	Image *p_dogImage = new Image();
+	TextMessage *hello = new TextMessage(10, "두들", "안녕");
+	ImageMessage *dog = new ImageMessage(20, "두들", p_dogImage);
+	
+	cout << "보낸 시간 : " << hello->GetSendTime() << endl;
+	cout << "보낸 시람 : " << hello->GetSendName() << endl;
+	cout << "보낸 내용 : " << hello->GetText() << endl;
+	cout << endl;
+	
+	cout << "보낸 시간 : " << dog->GetSendTime() << endl;
+	cout << "보낸 시람 : " << dog->GetSendName() << endl;
+	cout << "보낸 내용 : " << (string)*dog->GetImage() << endl;
+	cout << endl;
+	
+	delete hello;
+	delete dog;
+	delete p_dogImage;
+}
