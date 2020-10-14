2233faeed93f4356bfb374eb7a6cdad398c362c4
kwarkmc <kwarkmc@naver.com>
2020-10-14 12:03:59 +0000

    상속이 필요한 이유

--- 201014/ex1.cpp
+++ 201014/ex1.cpp
@@ -25,6 +25,10 @@ public:
 	
 	int GetSendTime() const {return sendTime;}
 	string GetSendName() const {return sendName;}
+	virtual string GetContent() const {
+		return "";
+	}
+	
 private:
 	int sendTime;
 	string sendName;
@@ -37,7 +41,8 @@ public:
 		this->text = text;
 	} 
 	
-	string GetText() const {return text;}
+	string GetText() const { return text; }
+	string GetContent() const { return text; }
 	
 private:
 	string text;
@@ -50,36 +55,53 @@ public:
 		this->image = image; //포인터 자체를 얕은 복사하는 것은 문제가 되지 않는다.
 	} 
 	Image *GetImage() const {return image;}
+	string GetContent() const { return (string)*image; }
+	
 private:
 	Image *image; //Image라는 클래스 자체가 ImageMessage 클래스 내로 들어가게 되면, ImageMessage 클래스가 아주 무거워지기 때문에, 포인터로 조작한다.
 };
 
-void printMessage(TextMessage *m) {
+void printMessage(Message *m) { //부모 포인터에 가상 함수가 있기 때문에 그 가상 함수를 오버로딩 해서 사용하면 한개의 함수로 Message 클래스에서 상속된 두 개의 메시지를 모두 받을 수 있다.
 	cout << "보낸 시간 : " << m->GetSendTime() << endl;
 	cout << "보낸 사람 : " << m->GetSendName() << endl;
-	cout << "보낸 내용 : " << m->GetText() << endl;
+	cout << "보낸 내용 : " << m->GetContent() << endl;
 	cout << endl;
 }
 
-void printMessage(ImageMessage *m) {
+/*void printMessage(ImageMessage *m) {
 	cout << "보낸 시간 : " << m->GetSendTime() << endl;
 	cout << "보낸 사람 : " << m->GetSendName() << endl;
 	cout << "보낸 내용 : " << (string)*m->GetImage() << endl;
 	cout << endl;
 }
-
+*/
 int main() {
 	// 두들 : "안녕"
 	// 두들 : "강아지 사진"
 	
 	Image *p_dogImage = new Image();
+	
+	Message *messages[] = {
+		new TextMessage(10, "두들", "안녕"),
+		new TextMessage(11, "두들", "안녕"),
+		new TextMessage(12, "두들", "안녕"),
+		new ImageMessage(20, "두들", p_dogImage),
+	}; //메시지를 가르키는 포인터의 배열을 만든다.
+	
+	
+	for (Message *m : messages) {
+		printMessage(m);
+	}
+	/*
+	for (int i = 0; i < 4; i++) {
+		printMessage(*messages[i]);
+	}
+	*/
+	
+	/*
 	TextMessage *hello = new TextMessage(10, "두들", "안녕");
 	ImageMessage *dog = new ImageMessage(20, "두들", p_dogImage);
+	*/
 	
-	printMessage(hello); // 바로 포인터값인 hello를 대입해주면 된다.
-	printMessage(dog);
-	
-	delete hello;
-	delete dog;
 	delete p_dogImage;
 }
