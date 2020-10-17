353c5b59de74f61a93256faba80a8fef8d98436a
kwarkmc <kwarkmc@naver.com>
2020-10-17 10:23:27 +0000

    201017 19:22 순수 가상 함수와 추상 클래스

--- 201017/ex2.cpp
+++ 201017/ex2.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 double PI = 3.1415926535;
 
-class Circle {
+class Shape {
+public:
+	virtual double GetArea() = 0;
+	
+	virtual void Resize(double f) = 0;
+private:
+	
+protected:
+	
+};
+
+class Circle : public Shape {
 public:
 	Circle(double r) : r(r) {}
 	
@@ -14,14 +25,14 @@ public:
 	
 	void Resize(double f) {
 		r *= f;
-	}
+	}	
 private:
 	double r;
 };
 
-class Rectangle {
+class Rectangle : public Shape {
 public:
-	Rectangle(double a, double b) : a(a), b(b), {}
+	Rectangle(double a, double b) : a(a), b(b) {}
 	
 	double GetArea() {
 		return a*b;
@@ -37,5 +48,20 @@ private:
 };
 
 int main() {
+	Shape *shapes[] = {
+		new Circle(10),
+		new Rectangle(20, 30)
+		//new Shape 순수 가상 함수를 가지고 있는 추상 클래스이므로 객체 생성이 되지 않는다.
+	};
+	for (Shape *s : shapes) {
+		s->Resize(2);
+	}
 	
+	for (Shape *s : shapes) {
+		cout << s->GetArea() << endl;
+	}
+	
+	for (Shape *s : shapes) {
+		delete s;
+	}
 }
