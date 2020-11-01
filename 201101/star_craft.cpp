ae168a3925dd02416a3bd359e83434d745568aac
kwarkmc <kwarkmc@naver.com>
2020-11-01 11:04:07 +0000

    CPP star_craft    201101 20:30

--- 201101/star_craft.cpp
+++ 201101/star_craft.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 class Marine {
 private:
@@ -7,9 +8,12 @@ private:
 	int coord_y;
 	int damage;
 	bool is_dead;
+	char* name;
+	
 public:
 	Marine();
 	Marine(int x, int y);
+	Marine(int x, int y, const char* marine_name);
 	
 	int attack();
 	void be_attacked(int damage_earn);
@@ -23,9 +27,22 @@ Marine::Marine() {
 	coord_x = coord_y = 0;
 	damage = 5;
 	is_dead = false;
+	name = NULL;
 }
 
 Marine::Marine(int x, int y) {
+	coord_x = x;
+	coord_y = y;
+	hp = 50;
+	damage = 5;
+	is_dead = false;
+	name = NULL;
+}
+
+Marine::Marine(int x, int y, const char* marine_name) {
+	name = new char[strlen(marine_name) + 1];
+	strcpy(name, marine_name);
+	
 	coord_x = x;
 	coord_y = y;
 	hp = 50;
@@ -50,13 +67,13 @@ void Marine::be_attacked(int damage_earn) {
 }
 
 void Marine::show_status() {
-	std::cout << "***Marine***" << std::endl;
+	std::cout << "***Marine : " << name << "***" << std::endl;
 	std::cout << " Location : ( " << coord_x << " , " << coord_y << " )" << std::endl;
 	std::cout << " HP : " << hp << std::endl;
 }
 
 int main() {
-	Marine marine1(2, 3);
+	/*Marine marine1(2, 3);
 	Marine marine2(3, 5);
 	
 	marine1.show_status();
@@ -67,4 +84,23 @@ int main() {
 	
 	marine1.show_status();
 	marine2.show_status();
+	*/
+	
+	Marine *marines[100];
+	
+	marines[0] = new Marine(2, 3, "Marine 2");
+	marines[1] = new Marine(3, 5, "Marine 1");
+	
+	marines[0]->show_status();
+	marines[1]->show_status();
+	
+	std::cout << std::endl << "마린 1이 마린 2를 공격!" << std::endl;
+	
+	marines[0]->be_attacked(marines[1]->attack());
+	
+	marines[0]->show_status();
+	marines[1]->show_status();
+	
+	delete marines[0];
+	delete marines[1];
 }
