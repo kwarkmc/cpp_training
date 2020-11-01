a089441ded71bbc542d93fc06395d68e32397ec1
kwarkmc <kwarkmc@naver.com>
2020-11-01 11:19:16 +0000

    CPP star_craft_marine 201101 20:18

---
+++ 201101/star_craft_marine.cpp
@@ -0,0 +1,116 @@
+//Marine
+
+#include <iostream>
+#include <string.h>
+
+class Marine {
+private:
+	int hp;
+	int coord_x;
+	int coord_y;
+	int damage;
+	bool is_dead;
+	char* name;
+	
+public:
+	Marine();
+	Marine(int x, int y);
+	Marine(int x, int y, const char* marine_name);
+	~Marine();
+	
+	int attack();
+	void be_attacked(int damage_earn);
+	void move(int x, int y);
+	
+	void show_status();
+};
+
+Marine::Marine() {
+	hp = 50;
+	coord_x = coord_y = 0;
+	damage = 5;
+	is_dead = false;
+	name = NULL;
+}
+
+Marine::Marine(int x, int y) {
+	coord_x = x;
+	coord_y = y;
+	hp = 50;
+	damage = 5;
+	is_dead = false;
+	name = NULL;
+}
+
+Marine::Marine(int x, int y, const char* marine_name) {
+	name = new char[strlen(marine_name) + 1];
+	strcpy(name, marine_name);
+	
+	coord_x = x;
+	coord_y = y;
+	hp = 50;
+	damage = 5;
+	is_dead = false;
+}
+
+Marine::~Marine() { // new char* name[strlen(marine_name) + 1]; 에서 할당해주었던 메모리를 해제하기 위해 사용하는 소멸자 (destructor)
+	std::cout << name << " 의 소멸자 호출!" << std::endl;
+	if(name != NULL) {
+		delete[] name;
+	}
+}
+
+void Marine::move(int x, int y) {
+	coord_x = x;
+	coord_y = y;
+}
+
+int Marine::attack() {
+	return damage;
+}
+
+void Marine::be_attacked(int damage_earn) {
+	hp -= damage_earn;
+	if(hp <= 0) {
+		is_dead = true;
+	}
+}
+
+void Marine::show_status() {
+	std::cout << "***Marine : " << name << "***" << std::endl;
+	std::cout << " Location : ( " << coord_x << " , " << coord_y << " )" << std::endl;
+	std::cout << " HP : " << hp << std::endl;
+}
+
+int main() {
+	/*Marine marine1(2, 3);
+	Marine marine2(3, 5);
+	
+	marine1.show_status();
+	marine2.show_status();
+	
+	std::cout << std::endl << "마린 1이 마린 2를 공격!" << std::endl;
+	marine2.be_attacked(marine1.attack());
+	
+	marine1.show_status();
+	marine2.show_status();
+	*/
+	
+	Marine *marines[100];
+	
+	marines[0] = new Marine(2, 3, "Marine 2");
+	marines[1] = new Marine(3, 5, "Marine 1");
+	
+	marines[0]->show_status();
+	marines[1]->show_status();
+	
+	std::cout << std::endl << "마린 1이 마린 2를 공격!" << std::endl;
+	
+	marines[0]->be_attacked(marines[1]->attack());
+	
+	marines[0]->show_status();
+	marines[1]->show_status();
+	
+	delete marines[0];
+	delete marines[1];
+}
