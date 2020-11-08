fc524d18b36ec40d7137b0619237531c8de85b58
kwarkmc <kwarkmc@naver.com>
2020-11-08 12:14:10 +0000

    CPP my_string 201108 21:13

--- 201108/my_string.cpp
+++ 201108/my_string.cpp
@@ -6,6 +6,8 @@ private:
 	char* string_content; //문자열 데이터를 가르키는 포인터
 	int string_length; // 문자열의 길이
 	
+	int memory_capacity;
+	
 public:
 	MyString(char c);
 	//문자 하나로 생성
@@ -15,9 +17,16 @@ public:
 	//복사 생성자
 	~MyString();
 	
-	int length() const;
 	void print() const;
 	void println() const;
+	
+	int length() const;
+	int capacity() const;
+	void reserve(int size);
+	
+	//assign 함수
+	MyString& assign(const MyString& str);
+	MyString& assign(const char* str);
 };
 
 MyString::MyString(char c) {
@@ -63,10 +72,69 @@ void MyString::println() const {
 	std::cout << std::endl;
 }
 
+MyString& MyString::assign(const MyString& str) {
+	if(str.string_length > memory_capacity) {
+		//할당을 다시 해주어야 한다. 원래 할당되었던 배열의 크기보다 크기 때문에.
+		delete[] string_content;
+		
+		string_content = new char[str.string_length];
+		memory_capacity = str.string_length;
+	}
+	
+	for (int i = 0; i != str.string_length; i++) {
+		string_content[i] = str.string_content[i];
+	}
+	
+	string_length = str.string_length;
+	
+	return *this;
+}
+
+MyString& MyString::assign(const char* str) {
+	int str_length = strlen(str);
+	if (str_length < memory_capacity) {
+		delete[] string_content;
+		
+		string_content = new char[str_length];
+		memory_capacity = str_length;
+	}
+	
+	for (int i = 0; i != str_length; i++) {
+		string_content[i] = str[i];
+	}
+	
+	string_length = str_length;
+	
+	return *this;
+}
+
+int MyString::capacity() const {
+	return memory_capacity;
+}
+
+void MyString::reserve(int size) {
+	if (size > memory_capacity) {
+		char *prev_string_content = string_content;
+		
+		string_content = new char[size];
+		memory_capacity = size;
+		
+		for (int i = 0; i != string_length; i++) {
+			string_content[i] = prev_string_content[i];
+		}
+		
+		delete[] prev_string_content;
+	}
+}
+
 int main() {
-	MyString str1("Hello world!");
-	MyString str2(str1);
+	MyString str1("very very very long string");
+	str1.reserve(30);
+	
+	std::cout << "Capacity : " << str1.capacity() << std::endl;
+	std::cout << "String length : " << str1.length() << std::endl;
 	
 	str1.println();
-	str2.println();
 }
+
+//4-5
